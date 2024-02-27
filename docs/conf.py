b28c53e12e2035343cb44491d243ad0fd7eaa9f9
Aristotelis Mikropoulos <amikrop@gmail.com>
2024-02-27 17:34:13 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-import aiomixcloud
+import aiomixcloud  # noqa
 
 # -- Project information -----------------------------------------------------
 
